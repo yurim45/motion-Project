3ae0bafa7dcd28418c9d86d6673ccafa814b3f81
yrkim <icho0405@gmail.com>
2022-01-02 10:26:43 +0900

    add: page component

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,8 @@
-"use strict";
+import { PageComponent } from "./components/pase.js";
+class App {
+    constructor(appRoot) {
+        this.page = new PageComponent();
+        this.page.attachTo(appRoot);
+    }
+}
+new App(document.querySelector(".document"));
---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import { PageComponent } from "./components/pase.js";
+
+class App {
+  private readonly page: PageComponent;
+  constructor(appRoot: HTMLElement) {
+    this.page = new PageComponent();
+    this.page.attachTo(appRoot);
+  }
+}
+
+new App(document.querySelector(".document")! as HTMLElement);
