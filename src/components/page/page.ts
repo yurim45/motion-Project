a123075ffb0ae9acfb7d976da55110d6b1ef45aa
yrkim <icho0405@gmail.com>
2022-01-04 08:52:24 +0900

    아이템 삭제 기능 구현 하기 💡

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -4,24 +4,34 @@ export interface Composable {
   addChild(child: Component): void;
 }
 
+type OnCloseListener = () => void;
+
 class PageItemComponent
   extends BaseComponent<HTMLElement>
   implements Composable
 {
+  private closeListener?: OnCloseListener;
   constructor() {
     super(`
     <li class="page-item">
       <section class="page-item__body"></section>
       <div class="page-item__controls">
-        <buttoon class="close">&times;</buttoon>
+        <button class="close">&times;</button>
       </div>
     </li>`);
+    const closeBtn = this.element.querySelector('.close')! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
   }
   addChild(child: Component) {
     const container = this.element.querySelector(
       '.page-item__body'
     )! as HTMLElement;
     child.attachTo(container);
+  }
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
   }
 }
 
@@ -36,5 +46,8 @@ export class PageComponent
     const item = new PageItemComponent();
     item.addChild(section);
     item.attachTo(this.element, 'beforeend');
+    item.setOnCloseListener(() => {
+      item.removeFrom(this.element);
+    });
   }
 }
