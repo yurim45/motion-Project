ad9793be5bf4c39e5858a96e425afe4d8096e814
yrkim <icho0405@gmail.com>
2022-01-04 08:37:47 +0900

    페이지 아이템 컨테이너 만들기 💡

--- dist/app.js
+++ dist/app.js
@@ -8,13 +8,13 @@ class App {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
         const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-        image.attachTo(appRoot, 'beforeend');
+        this.page.addChild(image);
         const video = new VideoComponent('Video Title', 'https://www.youtube.com/embed/0jhM2YIWxBA');
-        video.attachTo(appRoot, 'beforeend');
+        this.page.addChild(video);
         const note = new NoteComponent('Note Title', 'Note Body');
-        note.attachTo(appRoot, 'beforeend');
+        this.page.addChild(note);
         const todo = new TodoComponent('Todo Title', 'Todo Item');
-        todo.attachTo(appRoot, 'beforeend');
+        this.page.addChild(todo);
     }
 }
 new App(document.querySelector('.document'));
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,6 +1,40 @@
-import { BaseComponent } from '../component.js';
-export class PageComponent extends BaseComponent<HTMLUListElement> {
+import { BaseComponent, Component } from '../component.js';
+
+export interface Composable {
+  addChild(child: Component): void;
+}
+
+class PageItemComponent
+  extends BaseComponent<HTMLElement>
+  implements Composable
+{
   constructor() {
-    super('<ul class="page">This is PageComponent</ul>');
+    super(`
+    <li class="page-item">
+      <section class="page-item__body"></section>
+      <div class="page-item__controls">
+        <buttoon class="close">&times;</buttoon>
+      </div>
+    </li>`);
+  }
+  addChild(child: Component) {
+    const container = this.element.querySelector(
+      '.page-item__body'
+    )! as HTMLElement;
+    child.attachTo(container);
+  }
+}
+
+export class PageComponent
+  extends BaseComponent<HTMLUListElement>
+  implements Composable
+{
+  constructor() {
+    super('<ul class="page"></ul>');
+  }
+  addChild(section: Component) {
+    const item = new PageItemComponent();
+    item.addChild(section);
+    item.attachTo(this.element, 'beforeend');
   }
 }
